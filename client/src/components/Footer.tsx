99d6038b9fe115345ace83b7014b3bd4540f779e
Jaskeerat Bhatia <jaskeeratbhatia@62:5c:48:f1:23:fa.home>
2025-02-06 00:01:57 -0500

    feat : add landing page info

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,16 @@
+
+
+const Footer = () => <footer className="w-screen flex gap-6 flex-wrap items-center justify-center  mt-10 pt-20 pb-10 ">
+    <div className='bg-[linear-gradient(to_top,_rgba(128,0,128,0.3)_0%,_rgba(0,0,0,0)_80%)] blur-lg h-[280] w-[100%] z-0 absolute blur-lg w-[100%] z-0 absolute'>
+    </div>
+    <a
+        className="rounded-full border border-solid border-black/[.08] dark:border-white/[.5] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44 text-2xl font-bold z-10"
+        href=""
+        target="_blank"
+        rel="noopener noreferrer"
+    >
+        See where next unicorn can take you!
+    </a>
+</footer>
+
+export default Footer;
---
+++ client/src/components/Gradient.tsx
@@ -0,0 +1,8 @@
+const Gradient = () => {
+  return (
+    <div className="fixed top-20 flex justify-center w-[100%]">
+      <div className="w-[800px] h-[250px] bg-[radial-gradient(ellipse_at_top,_rgba(128,0,128,0.3)_0%,_rgba(0,0,0,0)_80%)] blur-lg z-1"></div>
+    </div>
+  );
+}
+export default Gradient;
