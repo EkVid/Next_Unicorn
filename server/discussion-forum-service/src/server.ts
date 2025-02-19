72ee154dec474ef324cbabde6f8bc950f48f4421
Verusins <115052147+Bunbunw@users.noreply.github.com>
2025-02-19 10:57:29 +0800

    MongoDB connection
    After backend starts now, is connected to MongoDB. Can test via http://localhost:5001/api/posts function.

--- server/discussion-forum-service/src/server.ts
+++ server/discussion-forum-service/src/server.ts
@@ -1,6 +1,17 @@
 import app from "./app";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/mydatabase";
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error("MongoDB Connection Error:", err));
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
