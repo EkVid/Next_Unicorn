59c2daf56b8adf3d6488a91a45c7403448c75cca
mh-anwar <mohammad.anwar2939@gmail.com>
2025-02-21 16:40:11 -0500

    feat: add dotenvx for environment-based vars

--- server/discussion-forum-service/src/server.ts
+++ server/discussion-forum-service/src/server.ts
@@ -1,4 +1,10 @@
+import mongoose from "mongoose";
 import app from "./app";
+import "@dotenvx/dotenvx/config";
+// Database connection
+mongoose.connect(process.env.MONGODB_CONNECTION!).then(() => {
+  console.log("MongoDB connected to: " + process.env.MONGODB_CONNECTION);
+});
 
 const PORT = process.env.PORT || 5001;
 
