91f5295876f6c9326f9dab86968b4f1dea8f1be6
mh-anwar <mohammad.anwar2939@gmail.com>
2025-02-21 01:50:41 -0500

    Add all CRUD posts routes

--- server/discussion-forum-service/src/routes/posts.routes.ts
+++ server/discussion-forum-service/src/routes/posts.routes.ts
@@ -1,10 +1,21 @@
 import express from "express";
-import { createPost, getAllPosts, getPostById } from "../controllers/posts.controller";
+import {
+  createPost,
+  deletePost,
+  getAllPosts,
+  getPostById,
+  getUserPosts,
+  updatePost,
+} from "../controllers/posts.controller";
 
 const router = express.Router();
 
 router.get("/posts", getAllPosts);
 router.get("/posts/:id", getPostById);
+router.get("/posts/user/:id", getUserPosts);
+
 router.post("/posts", createPost);
+router.patch("/posts/:id", updatePost);
+router.delete("/posts/:id", deletePost);
 
 export default router;
