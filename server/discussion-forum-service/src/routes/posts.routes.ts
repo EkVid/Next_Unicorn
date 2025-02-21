c4dcf34f04452ffc1bcbe3a9f56f1d8f9ee51f7d
Verusins <115052147+Bunbunw@users.noreply.github.com>
2025-02-21 20:55:39 +0800

    test

--- server/discussion-forum-service/src/routes/posts.routes.ts
+++ server/discussion-forum-service/src/routes/posts.routes.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { createPost, getAllPosts, getPostById } from "../controllers/posts.controller";
+import { createPost, getAllPosts, getPostById, deletePostById } from "../controllers/posts.controller";
 
 const router = express.Router();
 
 router.get("/posts", getAllPosts);
 router.get("/posts/:id", getPostById);
+router.delete("/posts/:id", deletePostById);
 router.post("/posts", createPost);
 
 export default router;
