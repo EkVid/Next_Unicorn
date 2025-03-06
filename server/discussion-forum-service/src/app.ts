11494e6ab889c4e4a00442a73b277e89f2c703e2
mh-anwar <mohammad.anwar2939@gmail.com>
2025-03-05 21:19:31 -0500

    feat: create Swagger UI with OpenAPI JSON spec

--- server/discussion-forum-service/src/app.ts
+++ server/discussion-forum-service/src/app.ts
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import postRoutes from "./routes/posts.routes";
 import commentRoutes from "./routes/comments.routes";
+import swaggerUi from "swagger-ui-express";
+import openApiSpec from "../swagger.json";
 
 const app = express();
 app.use(cors());
@@ -9,5 +11,5 @@ app.use(express.json());
 
 app.use("/api", postRoutes);
 app.use("/api", commentRoutes);
-
+app.use("/api-docs/", swaggerUi.serve, swaggerUi.setup(openApiSpec));
 export default app;
